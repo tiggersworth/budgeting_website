868b08dbebed492438ac3bd9c72b150d528e274f
Jordan Chau <jchau3@buffalo.edu>
2018-10-24 17:23:44 -0400

    input file button will be disabled after file is uploaded successfully to prevent potential bugs

--- trackExpenses.js
+++ trackExpenses.js
@@ -1,55 +1,65 @@
 var expenses = [['Charges', 'Percent of Budget']];
 
 document.getElementById('file').onchange = function(){
+  var fileDisplayArea = document.getElementById('fileDisplayArea');
+  var fileInput = document.getElementById('file');
 
   var file = this.files[0];
+  var textType = /text.*/;
 
-  var reader = new FileReader();
-  reader.onload = function(progressEvent){
-    // Entire file
-    //console.log(this.result);
+  if (file.type.match(textType)) {
+    var reader = new FileReader();
+    reader.onload = function(progressEvent){
+      // Entire file
+      //console.log(this.result);
 
-    // By lines
-    var lines = this.result.split('\n');
-    for(var line = 0; line < lines.length; line++){
-      ///console.log();
-      var data = lines[line];
+      // By lines
+      var lines = this.result.split('\n');
+      for(var line = 0; line < lines.length; line++){
+        ///console.log();
+        var data = lines[line];
 
-      if(data !== null && data !== '') {
-        var category_price = data.split(' ');
-        console.log(category_price[1]);
+        if(data !== null && data !== '') {
+          var category_price = data.split(' ');
+          console.log(category_price[1]);
 
-        var price = parseInt(category_price[1]);
-        category_price[1] = price;
-        expenses.push(category_price);
+          var price = parseInt(category_price[1]);
+          category_price[1] = price;
+          expenses.push(category_price);
+        }
       }
+
+      //console.log(expenses);
+    };
+    reader.readAsText(file);
+
+    // Load google charts
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    // Draw the chart and set the chart values
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable(expenses);/*[
+        ['Charges', 'Percent of Budget'],
+        ['Rent', 25],
+        ['Food', 15],
+        ['Gas', 5],
+        ['Phone Bill', 5],
+        ['Electric', 5],
+        ['Remaining', 45]
+      ] */
+
+      // Optional; add a title and set the width and height of the chart
+      var options = {'title':'Your Budget Summary', 'width':825, 'height':600};
+
+      // Display the chart inside the <div> element with id="piechart"
+      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+      chart.draw(data, options);
     }
 
-    console.log(expenses);
-  };
-  reader.readAsText(file);
-
-  // Load google charts
-  google.charts.load('current', {'packages':['corechart']});
-  google.charts.setOnLoadCallback(drawChart);
-
-  // Draw the chart and set the chart values
-  function drawChart() {
-    var data = google.visualization.arrayToDataTable(expenses);/*[
-      ['Charges', 'Percent of Budget'],
-      ['Rent', 25],
-      ['Food', 15],
-      ['Gas', 5],
-      ['Phone Bill', 5],
-      ['Electric', 5],
-      ['Remaining', 45]
-    ] */
-
-    // Optional; add a title and set the width and height of the chart
-    var options = {'title':'Your Budget Summary', 'width':825, 'height':600};
-
-    // Display the chart inside the <div> element with id="piechart"
-    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-    chart.draw(data, options);
+    fileInput.disabled = true;
+  } else {
+    fileDisplayArea.innerText = "File not supported!";
   }
+
 };
