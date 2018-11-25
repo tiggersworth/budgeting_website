1921ec8e30cb86360815e454902c51f3fd1b487e
Jordan Chau <jchau3@buffalo.edu>
2018-11-25 17:09:49 -0500

    implemented display graph button for displaying manual input expense values

--- trackExpenses.js
+++ trackExpenses.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         var fDesc = $("<font size=\"3\" class=\"fieldDescription\" >Expense Type</font>");
         var fName = $("<input type=\"text\" class=\"fieldname\" />");
         var fDesc2 = $("<font size=\"3\" class=\"fieldDescription\" >Cost</font>");
-        var fValue = $("<input type=\"text\" class=\"fieldname\" />");
+        var fValue = $("<input type=\"number\" class=\"fieldcost\" />");
         //var fType = $("<select class=\"fieldtype\"><option value=\"checkbox\">Checked</option><option value=\"textbox\">Text</option><option value=\"textarea\">Paragraph</option></select>");
         var removeButton = $("<input type=\"button\" class=\"remove\" value=\"-\" />");
         removeButton.click(function() {
@@ -22,13 +22,19 @@ $(document).ready(function() {
         $("#buildyourform").append(fieldWrapper);
     });
 
-    $("#preview").click(function() {
-        $("#yourform").remove();
-        var fieldSet = $("<fieldset id=\"yourform\"><legend>Your Form</legend></fieldset>");
+    var expenses;
+
+    $("#display").click(function() {
+        $("#piechart").empty();
+        expenses = [['Charges', 'Percent of Budget']];
+        //var fieldSet = $("<fieldset id=\"yourform\"><legend>Your Form</legend></fieldset>");
         $("#buildyourform div").each(function() {
-            var id = "input" + $(this).attr("id").replace("field","");
-            var label = $("<label for=\"" + id + "\">" + $(this).find("input.fieldname").first().val() + "</label>");
-            var input;
+            //var id = "input" + $(this).attr("id").replace("field","");
+            //var label = $("<label for=\"" + id + "\">" + $(this).find("input.fieldname").first().val() + "</label>");
+            var curExpense = [$(this).find("input.fieldname").first().val(), parseInt($(this).find("input.fieldcost").first().val())];
+            expenses.push(curExpense);
+
+            /*var input;
             switch ($(this).find("select.fieldtype").first().val()) {
                 case "checkbox":
                     input = $("<input type=\"checkbox\" id=\"" + id + "\" name=\"" + id + "\" />");
@@ -39,14 +45,16 @@ $(document).ready(function() {
                 case "textarea":
                     input = $("<textarea id=\"" + id + "\" name=\"" + id + "\" ></textarea>");
                     break;
-            }
-            fieldSet.append(label);
-            fieldSet.append(input);
+            } */
+            //fieldSet.append(label);
+            //fieldSet.append(input);
         });
-        $("body").append(fieldSet);
+        //$("body").append(fieldSet);
+        console.log(expenses);
+        // Load google charts
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
     });
-
-    var expenses = [['Charges', 'Percent of Budget']];
 
     document.getElementById('file').onchange = function(){
       var fileDisplayArea = document.getElementById('fileDisplayArea');
@@ -54,10 +62,13 @@ $(document).ready(function() {
 
       var file = this.files[0];
       var textType = /text.plain/; //.* means everything so changed to .plain
-      console.log(file.type);
+      //console.log(file.type);
 
       if (file.type.match(textType)) {
         var reader = new FileReader();
+        $("#piechart").empty();
+        expenses = [['Charges', 'Percent of Budget']];
+
         reader.onload = function(progressEvent){
           // Entire file
           //console.log(this.result);
@@ -70,7 +81,7 @@ $(document).ready(function() {
 
             if(data !== null && data !== '') {
               var category_price = data.split(' ');
-              console.log(category_price[1]);
+              //console.log(category_price[1]);
 
               var price = parseInt(category_price[1]);
               category_price[1] = price;
@@ -78,33 +89,13 @@ $(document).ready(function() {
             }
           }
 
-          //console.log(expenses);
+          console.log(expenses);
         };
         reader.readAsText(file);
 
         // Load google charts
         google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
-
-        // Draw the chart and set the chart values
-        function drawChart() {
-          var data = google.visualization.arrayToDataTable(expenses);/*[
-            ['Charges', 'Percent of Budget'],
-            ['Rent', 25],
-            ['Food', 15],
-            ['Gas', 5],
-            ['Phone Bill', 5],
-            ['Electric', 5],
-            ['Remaining', 45]
-          ] */
-
-          // Optional; add a title and set the width and height of the chart
-          var options = {'title':'Your Budget Summary', 'width':825, 'height':600};
-
-          // Display the chart inside the <div> element with id="piechart"
-          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-          chart.draw(data, options);
-        }
 
         fileInput.disabled = true;
         fileDisplayArea.innerText = "";
@@ -113,4 +104,24 @@ $(document).ready(function() {
       }
 
     };
+
+    // Draw the chart and set the chart values
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable(expenses);/*[
+        ['Charges', 'Percent of Budget'],
+        ['Rent', 25],
+        ['Food', 15],
+        ['Gas', 5],
+        ['Phone Bill', 5],
+        ['Electric', 5],
+        ['Remaining', 45]
+      ] */
+
+      // Optional; add a title and set the width and height of the chart
+      var options = {'title':'Your Budget Summary', 'width':825, 'height':600};
+
+      // Display the chart inside the <div> element with id="piechart"
+      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+      chart.draw(data, options);
+    }
 });
