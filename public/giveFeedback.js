7185d87e21e1af021c4be36a662379c2d799232c
Jordan Chau <jchau3@buffalo.edu>
2018-12-02 18:01:43 -0500

    implemented email function for give feedback form

--- public/giveFeedback.js
+++ public/giveFeedback.js
@@ -1,3 +1,19 @@
-function myFunction(){
-  confirm("Submission received!");
+function sendEmail(){
+
+  if(checkMissingFields()) {
+
+  var templateParams = {
+    name: $('#name').val(),
+    comment: $('#comment').val(),
+    email: $('#email').val()
+};
+
+emailjs.send('gmail', 'template_lyD1lTsf', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
+  confirm("Submission received!"); }
 }
