f72c8decfd3d09e76bf1b850ce47fd1e6cc9b6e8
Jordan Chau <jchau3@buffalo.edu>
2018-12-02 18:02:26 -0500

    added field validation for give feedback form and email validation for email address input

--- public/giveFeedback.js
+++ public/giveFeedback.js
@@ -17,3 +17,24 @@ emailjs.send('gmail', 'template_lyD1lTsf', templateParams)
 
   confirm("Submission received!"); }
 }
+
+function checkMissingFields(){
+  var name = document.getElementById("name").value;
+  var comment = document.getElementById("comment").value;
+  var email = document.getElementById("email").value;
+
+  if(name == "" || comment == "" || email == "") {
+    alert("Please fill in the missing fields.");
+    return false;
+  } else if(!validateEmail(email)) {
+    alert("Please provide a valid email.");
+    return false;
+  }
+
+  return true;
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
